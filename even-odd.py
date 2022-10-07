7a884d81d8fe8a8c1aa9d109b49199f13c5c84f8
vaibhav159166 <112896710+vaibhav159166@users.noreply.github.com>
2022-10-07 19:03:42 +0530

    Add files via upload

---
+++ even-odd.py
@@ -0,0 +1,6 @@
+# A Python program to find whether a given number (accept from t user) is even or odd
+n = int(input("Enter a number: "))  
+if (n % 2) == 0:  
+   print("It is Even number")  
+else:  
+   print("It is Odd number")
